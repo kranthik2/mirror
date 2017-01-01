7a5c914598733a5860cd9852f2e2b5f02e78ec88
Kulli <fze053@14109fded6b5.capitalone.com>
2017-01-01 16:11:12 -0600

    moved services to factory

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/mirror'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
-app.get('/', function(req, res){
-  res.redirect('/mirror/index.html');
+app.get('/', function(req, res) {
+    res.redirect('/mirror/index.html');
 });
 
 
